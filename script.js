b815577ecdb46d7a6eaa7b70616beed8173186a6
Valmarinoa <valmarinocol@gmail.com>
2021-07-30 16:17:52 +0200

    added onmouseover and out functions to all elements

---
+++ script.js
@@ -0,0 +1,71 @@
+function showFrozen() {
+  document.getElementById("frozen-img").style.display = "block";
+}
+
+function hideFrozen() {
+  document.getElementById("frozen-img").style.display = "none";
+}
+
+function showValue() {
+  document.getElementById("value-img").style.display = "block";
+}
+
+function hideValue() {
+  document.getElementById("value-img").style.display = "none";
+}
+
+function showAurae() {
+  document.getElementById("aurae-img").style.display = "block";
+}
+
+function hideAurae() {
+  document.getElementById("aurae-img").style.display = "none";
+}
+
+function showWater() {
+  document.getElementById("water-img").style.display = "block";
+}
+
+function hideWater() {
+  document.getElementById("water-img").style.display = "none";
+}
+
+function showThoughts() {
+  document.getElementById("thoughts-img").style.display = "block";
+}
+
+function hideThoughts() {
+  document.getElementById("thoughts-img").style.display = "none";
+}
+
+function showStar() {
+  document.getElementById("star-img").style.display = "block";
+}
+
+function hideStar() {
+  document.getElementById("star-img").style.display = "none";
+}
+
+function showBox() {
+  document.getElementById("box-img").style.display = "block";
+}
+
+function hideBox() {
+  document.getElementById("box-img").style.display = "none";
+}
+
+function showHumanSystems() {
+  document.getElementById("human-systems-img").style.display = "block";
+}
+
+function hideHumanSystems() {
+  document.getElementById("human-systems-img").style.display = "none";
+}
+
+function showSilence() {
+  document.getElementById("silence-img").style.display = "block";
+}
+
+function hideSilence() {
+  document.getElementById("silence-img").style.display = "none";
+}
